b8694faae62f8e207be1cf59f035446756d49c30
GyozaGuy <gyozaguy@gmail.com>
2023-01-28 10:13:05 -0700

    Engine config, added helpers

--- app/helpers/dumpling/application_helper.rb
+++ app/helpers/dumpling/application_helper.rb
@@ -1,4 +1,11 @@
 module Dumpling
   module ApplicationHelper
+    def component(name, *args, &block)
+      render_component(name, *args, &block)
+    end
+
+    def title(content)
+      content_for(:title, content)
+    end
   end
 end
--- lib/dumpling/engine.rb
+++ lib/dumpling/engine.rb
@@ -2,8 +2,8 @@ module Dumpling
   class Engine < ::Rails::Engine
     isolate_namespace Dumpling
 
-    paths['app/controllers'].concat(Dir.glob('app/views/*', base: root))
-    paths['app/helpers'] << root.join('app', 'components')
-    config.assets.paths << root.join('app', 'views')
+    initializer 'dumpling.assets' do
+      Rails.application.config.assets.paths << root.join('app', 'views')
+    end
   end
 end
