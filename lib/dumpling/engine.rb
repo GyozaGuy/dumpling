d2da3b14af1c78c7bd765d8981ecb2f173be2d20
GyozaGuy <gyozaguy@gmail.com>
2023-01-27 17:50:55 -0700

    Updated app config

--- lib/dumpling/engine.rb
+++ lib/dumpling/engine.rb
@@ -1,5 +1,9 @@
 module Dumpling
   class Engine < ::Rails::Engine
     isolate_namespace Dumpling
+
+    paths['app/controllers'].concat(Dir.glob('app/views/*', base: root))
+    paths['app/helpers'] << root.join('app', 'components')
+    config.assets.paths << root.join('app', 'views')
   end
 end
