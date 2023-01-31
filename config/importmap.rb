7b384553af08e6584d0c4d4e426578737ba16e2f
GyozaGuy <gyozaguy@gmail.com>
2023-01-30 17:22:17 -0700

    Fixed importmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,2 +1,2 @@
-pin 'application', to: 'dumpling/application.js', preload: true
+pin 'dumpling', to: 'dumpling/application.js', preload: true
 pin '@hotwired/stimulus', to: 'https://ga.jspm.io/npm:@hotwired/stimulus@3.2.1/dist/stimulus.js'
