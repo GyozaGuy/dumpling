7b8dab1b01e3c9df3b2222c20c5fd7099de5d8a1
GyozaGuy <gyozaguy@gmail.com>
2023-01-30 17:27:14 -0700

    Moved components into their own directory; updated component helper method

--- app/helpers/dumpling/application_helper.rb
+++ app/helpers/dumpling/application_helper.rb
@@ -1,6 +1,8 @@
 module Dumpling
   module ApplicationHelper
     def component(name, *args, &block)
+      render("components/#{name}/#{name}", *args, &block)
+    rescue StandardError
       render("#{name}/#{name}", *args, &block)
     end
 
