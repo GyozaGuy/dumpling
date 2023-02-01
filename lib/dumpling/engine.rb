97f5bf79e18231ec0f1d03313b4e1f61a44ab2a7
GyozaGuy <gyozaguy@gmail.com>
2023-02-01 11:28:52 -0700

    Updated engine config to eliminate most customizations at the app level

--- lib/dumpling/engine.rb
+++ lib/dumpling/engine.rb
@@ -2,25 +2,51 @@ module Dumpling
   class Engine < ::Rails::Engine
     isolate_namespace Dumpling
 
+    initializer 'dumpling.app_assets' do |app|
+      app.config.assets.paths << app.root.join('app', 'components')
+      app.config.assets.paths << app.root.join('app', 'views')
+    end
+
     initializer 'dumpling.assets' do |app|
       app.config.assets.paths << root.join('app', 'components')
       app.config.assets.paths << root.join('app', 'javascript')
       app.config.assets.paths << root.join('app', 'views')
     end
 
+    initializer 'dumpling.app_controllers' do |app|
+      app.config.paths['app/controllers'].concat(Dir.glob('app/views/*', base: app.root))
+    end
+
+    initializer 'dumpling.controllers' do
+      config.paths['app/controllers'].concat(Dir.glob('app/views/*', base: root))
+    end
+
     initializer 'dumpling.importmap', before: 'importmap' do |app|
       app.config.importmap.paths << root.join('config/importmap.rb')
-      app.config.importmap.cache_sweepers << root.join('app/javascript')
+    end
+
+    initializer 'dumpling.load_components', before: :set_autoload_paths do |app|
+      app.config.autoload_paths += Dir["#{app.root.join('app', 'components')}/{*}"]
+      app.config.autoload_paths += Dir["#{root.join('app', 'components')}/{*}"]
     end
 
     initializer 'dumpling.precompile' do |app|
-      app.config.assets.precompile += %w[dumpling_manifest]
+      app.config.assets.precompile << %w[dumpling_manifest]
+    end
+
+    initializer 'dumpling.app_view_paths' do |app|
+      components_path = app.root.join('app', 'components')
+
+      ActiveSupport.on_load :action_controller do
+        append_view_path components_path
+      end
     end
 
     initializer 'dumpling.view_paths' do
-      component_path = root.join('app', 'components')
+      components_path = root.join('app', 'components')
+
       ActiveSupport.on_load :action_controller do
-        append_view_path component_path
+        append_view_path components_path
       end
     end
   end
