12acb0e358beee6c4dcceaa63d72c3a52938ccfa
GyozaGuy <gyozaguy@gmail.com>
2023-02-07 22:45:57 -0700

    Fixed bug where multiple tabs components on the same page would interfere with each other

--- app/components/tabs/tabs_component.rb
+++ app/components/tabs/tabs_component.rb
@@ -1,26 +1,28 @@
 class TabsComponent < ViewComponent::Base
-  attr_reader :selected_index
+  attr_reader :selected_index, :tabs_id
 
   renders_many :tabs,
     lambda { |attrs, &block|
-      { attrs:, block:, component_class: TabComponent }
+      { attrs:, block:, component_class: TabComponent, tabs_id: }
     }
 
   def initialize(selected_index: 0)
     @selected_index = selected_index
+    @tabs_id = SecureRandom.uuid
   end
 
   class TabComponent < ViewComponent::Base
-    attr_reader :index, :label, :selected
+    attr_reader :index, :label, :selected, :tabs_id
 
-    def initialize(index:, label:, selected: false)
+    def initialize(index:, label:, tabs_id:, selected: false)
       @index = index
       @label = label
       @selected = selected
+      @tabs_id = tabs_id
     end
 
     def selected?
-      @selected == true
+      selected == true
     end
   end
 end
