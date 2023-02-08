2b45764956f2abc32af4c6826b9a87ded5b5cc42
GyozaGuy <gyozaguy@gmail.com>
2023-02-08 09:01:40 -0700

    Adjusted sidebar background color

--- app/components/sidebar/sidebar_component.rb
+++ app/components/sidebar/sidebar_component.rb
@@ -1,4 +1,4 @@
 class SidebarComponent < Dumpling::Component
-  property :background_color, default: 'hsl(210, 20%, 90%)'
+  property :background_color, default: 'hsl(200, 10%, 90%)'
   property :title
 end
