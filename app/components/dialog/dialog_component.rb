f681caeb48a67e3f2bdf9ad4b4f2868736984d1a
GyozaGuy <gyozaguy@gmail.com>
2023-02-09 23:14:36 -0700

    Implemented movable dialogs

--- app/components/dialog/dialog_component.rb
+++ app/components/dialog/dialog_component.rb
@@ -1,18 +1,29 @@
 class DialogComponent < ViewComponent::Base
-  attr_reader :close_on_click_scrim, :id, :modal, :title
+  attr_reader :id, :title
 
   renders_one :footer
   renders_one :open_button
 
-  def initialize(close_on_click_scrim: true, id: nil, modal: true, showing: false, title: nil)
+  def initialize(
+    close_on_click_scrim: true,
+    id: nil,
+    modal: true,
+    movable: true,
+    showing: false,
+    title: nil
+  )
     @classes = []
+    @scrim_classes = ['dialog_scrim']
 
     @close_on_click_scrim = close_on_click_scrim
     @id = id
     @modal = modal
+    @movable = movable
     @title = title
 
-    @classes.push('dialog-opening', 'dialog-visible') if showing
+    @classes.push('dialog-movable') if movable
+    @classes.push('dialog-visible') if showing
+    @scrim_classes.push('dialog-visible') if showing
   end
 
   def class_string
@@ -20,11 +31,19 @@ class DialogComponent < ViewComponent::Base
   end
 
   def close_on_click_scrim?
-    close_on_click_scrim == true
+    @close_on_click_scrim == true
+  end
+
+  def scrim_class_string
+    raw "class=\"#{@scrim_classes.join(' ')}\""
   end
 
   def modal?
-    modal == true
+    @modal == true
+  end
+
+  def movable?
+    @movable == true
   end
 
   def svg(path)
--- app/javascript/dumpling/controllers/dialog_component_controller.js
+++ app/javascript/dumpling/controllers/dialog_component_controller.js
@@ -5,6 +5,14 @@ import { wait } from '../helpers/utilities';
 export default class extends Controller {
   static classes = ['closing', 'opening', 'visible'];
   static targets = ['dialog', 'scrim'];
+  static values = { startX: Number, startY: Number };
+
+  beginMove({ clientX, clientY }) {
+    const [x, y] = this.dialogCoords;
+    this.startXValue = clientX - x;
+    this.startYValue = clientY - y;
+    document.body.addEventListener('mousemove', this.handleMouseMove);
+  }
 
   async close() {
     this.dialogTarget.classList.add(this.closingClass);
@@ -13,7 +21,7 @@ export default class extends Controller {
       this.scrimTarget.classList.add(this.closingClass);
     }
 
-    await wait(124); // NOTE: This is shorter than the animation duration on purpose
+    await wait(120); // NOTE: This is shorter than the animation duration on purpose
 
     this.dialogTarget.classList.remove(this.visibleClass, this.closingClass);
 
@@ -21,6 +29,18 @@ export default class extends Controller {
       this.scrimTarget.classList.remove(this.visibleClass, this.closingClass);
     }
   }
+
+  endMove() {
+    document.body.removeEventListener('mousemove', this.handleMouseMove);
+  }
+
+  handleMouseMove = ({ clientX, clientY }) => {
+    const newX = (this.startXValue - clientX) * -1;
+    const newY = (this.startYValue - clientY) * -1;
+
+    this.dialogTarget.style.setProperty('--dialog-translate-x', `${newX}px`);
+    this.dialogTarget.style.setProperty('--dialog-translate-y', `${newY}px`);
+  };
 
   async open() {
     this.dialogTarget.classList.add(this.openingClass, this.visibleClass);
@@ -36,5 +56,15 @@ export default class extends Controller {
     if (this.hasScrimTarget) {
       this.scrimTarget.classList.remove(this.openingClass);
     }
+  }
+
+  get dialogCoords() {
+    const x = (
+      parseInt(this.dialogTarget.style.getPropertyValue('--dialog-translate-x')) || 0
+    );
+    const y = (
+      parseInt(this.dialogTarget.style.getPropertyValue('--dialog-translate-y')) || 0
+    );
+    return [x, y];
   }
 }
