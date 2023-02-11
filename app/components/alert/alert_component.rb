c0535d6ec1b9939bc722db4254445137c15df67f
GyozaGuy <gyozaguy@gmail.com>
2023-02-11 10:36:16 -0700

    Converted alert to ViewComponent

--- app/components/alert/alert_component.rb
+++ app/components/alert/alert_component.rb
@@ -1,10 +1,23 @@
-class AlertComponent < Dumpling::Component
-  property :dismissable, default: true
-  property :type
+class AlertComponent < ViewComponent::Base
+  attr_reader :dismissable
+
+  def initialize(dismissable: true, type: nil)
+    @classes = ['alert']
+
+    @dismissable = dismissable
+
+    @classes << "alert-#{type}" if type
+  end
 
   def class_string
-    classes = ['alert']
-    classes << "alert-#{type}" unless type.nil?
-    classes.join(' ')
+    raw @classes.join(' ')
+  end
+
+  def dismissable?
+    @dismissable == true
+  end
+
+  def svg(path)
+    render inline: Dumpling::Engine.root.join('app', 'assets', 'images', 'dumpling', path).read
   end
 end
