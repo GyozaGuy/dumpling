8dd70b158656d67c2f6bb03d438560baf8148253
GyozaGuy <gyozaguy@gmail.com>
2023-02-11 11:56:02 -0700

    Added alias

--- app/helpers/dumpling/application_helper.rb
+++ app/helpers/dumpling/application_helper.rb
@@ -1,5 +1,12 @@
 module Dumpling
   module ApplicationHelper
+    def component(name, *args, **kwargs, &block)
+      component = "#{name.to_s.camelize}Component".safe_constantize
+      render component.new(*args, **kwargs), &block
+    end
+
+    alias vc component
+
     def page_title(content)
       content_for(:title, content)
     end
@@ -9,11 +16,6 @@ module Dumpling
       render inline: Rails.root.join('app', 'assets', 'images', path).read
     rescue StandardError
       render inline: Engine.root.join('app', 'assets', 'images', path).read
-    end
-
-    def vc(name, *args, **kwargs, &block)
-      component = "#{name.to_s.camelize}Component".safe_constantize
-      render component.new(*args, **kwargs), &block
     end
   end
 end
