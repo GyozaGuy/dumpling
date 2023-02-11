a5463485168e695b630e1db5369efe65af933087
GyozaGuy <gyozaguy@gmail.com>
2023-02-11 12:17:59 -0700

    Removed sidebar title

--- app/components/sidebar/sidebar_component.rb
+++ app/components/sidebar/sidebar_component.rb
@@ -1,7 +1,7 @@
 class SidebarComponent < Dumpling::Component
   attr_reader :title
 
-  def initialize(title:, background_color: 'hsl(200, 10%, 90%)')
+  def initialize(background_color: 'hsl(200, 10%, 90%)', title: nil)
     styles << "--sidebar-background-color: #{background_color}"
     @title = title
   end
