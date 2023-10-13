f89fbeaf9debe277f1abd7dceacadca52a44d109
GyozaGuy <gyozaguy@gmail.com>
2023-10-12 23:36:16 -0600

    Converted Input

--- app/views/components/input.rb
+++ app/views/components/input.rb
@@ -1,41 +1,42 @@
+VALID_INPUT_TYPES = %w[email date number password tel text time].freeze
+
 class Input < ApplicationComponent
   include Shared::IsColor
   include Shared::IsRounded
   include Shared::IsSize
 
   # TODO: finish icons
-  attr_accessor(
-    :disabled,
-    :left_icon,
-    :loading,
-    :placeholder,
-    :readonly,
-    :right_icon,
-    :state,
-    :static,
-    :type,
-    :value
-  )
+  attribute :disabled, :boolean
+  attribute :left_icon, :string
+  attribute :loading, :boolean
+  attribute :placeholder, :string
+  attribute :readonly, :boolean
+  attribute :right_icon, :string
+  attribute :static, :boolean
+  attribute :type, :string, default: 'text'
+  attribute :value
 
   def template
+    raise "Invalid input type: #{type}" unless VALID_INPUT_TYPES.include?(type)
+
     div(**classes('control', loading?: 'is-loading')) {
       input(
         **class_list(
           'input',
           static?: 'is-static'
         ),
-        disabled: @disabled ? '' : nil,
-        placeholder: @placeholder,
-        readonly: @readonly ? '' : nil,
-        type: @type || 'text',
-        value: @value
+        disabled: disabled ? '' : nil,
+        placeholder: placeholder,
+        readonly: readonly ? '' : nil,
+        type: type,
+        value: value
       )
     }
   end
 
   private
 
-  def loading? = @loading == true
+  def loading? = loading == true
 
-  def static? = @static == true
+  def static? = static == true
 end
