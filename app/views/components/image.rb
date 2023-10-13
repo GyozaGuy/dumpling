e258e1b55e14b16b6cd237bf2d68b22545c943dc
GyozaGuy <gyozaguy@gmail.com>
2023-10-12 23:20:21 -0600

    Moved constant for efficiency

--- app/views/components/image.rb
+++ app/views/components/image.rb
@@ -1,3 +1,5 @@
+VALID_IMAGE_SIZES = %w[16x16 24x24 32x32 48x48 64x64 96x96 128x128].freeze
+
 class Image < ApplicationComponent
   include Shared::HasRatio
   include Shared::IsRounded
@@ -5,8 +7,6 @@ class Image < ApplicationComponent
   attribute :alt, :string
   attribute :size, :string
   attribute :src, :string
-
-  VALID_IMAGE_SIZES = %w[16x16 24x24 32x32 48x48 64x64 96x96 128x128].freeze
 
   def template
     # TODO: figure out how to avoid certain things being auto-added while staying DRY (e.g. ratio)
