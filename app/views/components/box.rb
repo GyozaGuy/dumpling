891e56c11544b9010d8a3e048063b4a98d76ec64
GyozaGuy <gyozaguy@gmail.com>
2023-10-18 16:40:48 -0600

    Improved Box to allow it to behave as a link

--- app/views/components/box.rb
+++ app/views/components/box.rb
@@ -1,5 +1,13 @@
 class Box < ApplicationComponent
+  attribute :href, :string
+
   def template(&block)
-    div(class: 'box', &block)
+    box_element.call(class: 'box', href: href, &block)
+  end
+
+  private
+
+  def box_element
+    href.present? ? method(:a) : method(:div)
   end
 end
