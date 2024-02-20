a88bb7b1f390ffc4b9b4d2a0eceba8d0209eeb39
GyozaGuy <gyozaguy@gmail.com>
2024-02-19 19:52:08 -0700

    Updated importmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,5 +1,4 @@
 dumpling_root = Dumpling::Engine.root
-components_path = dumpling_root.join('app', 'components')
 dumpling_path = dumpling_root.join('app', 'javascript', 'dumpling')
 
 pin 'dumpling', to: 'dumpling/application.js', preload: true
@@ -8,7 +7,6 @@ pin(
   to: 'https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.14.0/cdn/shoelace.js',
   preload: true
 )
-pin_all_from components_path, under: 'dumpling/components'
+pin_all_from dumpling_path.join('components'), under: 'dumpling/components'
 pin_all_from dumpling_path.join('controllers'), under: 'dumpling/controllers'
 pin_all_from dumpling_path.join('helpers'), under: 'dumpling/helpers'
-pin_all_from 'app/components', under: 'components'
