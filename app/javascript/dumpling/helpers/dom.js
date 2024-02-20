75380ba8263841a2c0acd9a83f54809b7a9c2129
GyozaGuy <gyozaguy@gmail.com>
2024-02-19 19:54:27 -0700

    Removed unused files

--- app/javascript/dumpling/helpers/dom.js
+++
@@ -1,6 +0,0 @@
-export function html(htmlStrings, ...parts) {
-  const htmlString = htmlStrings.reduce((acc, cur, i) => `${acc}${cur}${parts[i] || ''}`, '');
-  const template = document.createElement('template');
-  template.innerHTML = htmlString;
-  return template.content;
-}
