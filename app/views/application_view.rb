7090ff2a8edc66c2779559fe8b0a98224dd8c0f3
GyozaGuy <gyozaguy@gmail.com>
2024-03-07 23:31:55 -0700

    Make all view context instance variables available in view

--- app/views/application_view.rb
+++ app/views/application_view.rb
@@ -4,4 +4,10 @@ class ApplicationView < ApplicationComponent
   # By default, it inherits from `ApplicationComponent`, but you
   # can change that to `Phlex::HTML` if you want to keep views and
   # components independent.
+
+  def template
+    @_view_context.instance_variables.each do |variable|
+      instance_variable_set(variable, @_view_context.instance_variable_get(variable))
+    end
+  end
 end
