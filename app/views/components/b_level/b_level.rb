a3b21412d2581ac5e6394a6fd0a384b6e6a91220
GyozaGuy <gyozaguy@gmail.com>
2024-03-20 11:27:20 -0600

    Improved BLevel

--- app/views/components/b_level/b_level.rb
+++ app/views/components/b_level/b_level.rb
@@ -1,11 +1,19 @@
 class BLevel < ApplicationComponent
   attribute :mobile, default: false
 
-  def item(&block) = div(class: 'level-item', &block)
+  def item(...) = render BLevelItem.new(...)
   def left(&block) = div(class: 'level-left', &block)
   def right(&block) = div(class: 'level-right', &block)
 
   def template(&block)
     div(**default_attributes, **classes('level', class_string, mobile?: 'is-mobile'), &block)
+  end
+
+  class BLevelItem < ApplicationComponent
+    attribute :narrow, default: false
+
+    def template(&block)
+      div(**classes('level-item', narrow?: 'is-narrow'), &block)
+    end
   end
 end
