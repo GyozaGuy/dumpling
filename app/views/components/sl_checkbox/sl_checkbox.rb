363db46028482ec9ad92fbf8717fe82f4b68001a
GyozaGuy <gyozaguy@gmail.com>
2024-04-06 21:39:33 -0600

    Add value attribute to SlCheckbox

--- app/views/components/sl_checkbox/sl_checkbox.rb
+++ app/views/components/sl_checkbox/sl_checkbox.rb
@@ -4,9 +4,13 @@ class SlCheckbox < ApplicationComponent
   attribute :indeterminate, default: false
   attribute :name
   attribute :size, default: :medium, one_of: %i[small medium large]
+  attribute :value
   register_element :sl_checkbox
 
   def template(&block)
-    sl_checkbox(**default_attributes, checked:, disabled:, indeterminate:, name:, size:, &block)
+    sl_checkbox(
+      **default_attributes, checked:, disabled:, indeterminate:, name:, size:, value:,
+      &block
+    )
   end
 end
