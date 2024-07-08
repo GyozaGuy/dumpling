eb38adea3e66bca9e408c44aaba9a4fd285149eb
GyozaGuy <gyozaguy@gmail.com>
2024-07-08 08:47:56 -0600

    Refactored BLevel

--- app/views/components/b_level/b_level.rb
+++ app/views/components/b_level/b_level.rb
@@ -2,8 +2,8 @@ class BLevel < ApplicationComponent
   attribute :mobile, default: false
 
   def item(...) = render BLevelItem.new(...)
-  def left(&block) = div(class: 'level-left', &block)
-  def right(&block) = div(class: 'level-right', &block)
+  def left(...) = render BLevelLeft.new(...)
+  def right(...) = render BLevelRight.new(...)
 
   def view_template(&block)
     div(**default_attributes, **classes('level', class_string, mobile?: 'is-mobile'), &block)
@@ -14,6 +14,18 @@ class BLevel < ApplicationComponent
 
     def view_template(&block)
       div(**classes('level-item', narrow?: 'is-narrow'), &block)
+    end
+  end
+
+  class BLevelLeft < ApplicationComponent
+    def view_template(&block)
+      div(**default_attributes, **classes('level-left', class_string), &block)
+    end
+  end
+
+  class BLevelRight < ApplicationComponent
+    def view_template(&block)
+      div(**default_attributes, **classes('level-right', class_string), &block)
     end
   end
 end
